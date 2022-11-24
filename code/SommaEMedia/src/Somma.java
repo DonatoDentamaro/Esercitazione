20daba3d00a454ba029a2a998a2795b31157f75e
DonatoDentamaro <donatodentamaro99@gmail.com>
2022-11-24 15:11:49 +0100

    SommaEMedia

---
+++ code/SommaEMedia/src/Somma.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Somma {
+    public static void main(String[] args)  {
+
+Scanner input = new Scanner(System.in);
+System.out.println("inserisci due numeri interi");
+int n1,n2;
+int somma;
+double media;
+
+ n1 =input.nextInt();
+ n2 =input.nextInt();
+ somma= n1+n2;
+ System.out.println("somma : ");
+        System.out.println(somma);
+
+ media= (somma/2);
+        System.out.println("media : ");
+                System.out.println(somma/2);
+
+        }
+
+
+    }
