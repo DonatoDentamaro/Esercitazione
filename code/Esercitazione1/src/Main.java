815679df4eb1562e44b7ed98903157db42269d1c
DonatoDentamaro <donatodentamaro99@gmail.com>
2022-11-24 12:56:51 +0100

    Nome

--- code/Esercitazione1/src/Main.java
+++ code/Esercitazione1/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(args["hello"]);
+        System.out.println("\nhello");
+        System.out.println("\nDonato Dentamaro");
     }
 
 
